b9ac9a8d74494fd46a253f78dd5f56fb8e40c3ca
ThiagoMSM <tguigomarques@gmail.com>
2025-12-04 15:06:43 -0300

    primeira commit

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using Domain.ValueObjects;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; }
+        public Cpf Cpf { get; private set; }
+        private User(Guid id, string name, string email, string passwordHash, Cpf cpf)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            PasswordHash = passwordHash;
+            Cpf = cpf;
+        }
+    }
+}
